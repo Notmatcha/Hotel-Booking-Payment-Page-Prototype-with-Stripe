10e9866f7df6bd5f6ccbaf9b1f02546edb28816e
Notmatcha <ngdingjie3@gmail.com>
2025-08-01 02:14:33 +0800

    middleware issues fixed

--- backend/routes/paymentRoutes.js
+++ backend/routes/paymentRoutes.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const paymentController = require("../controllers/paymentController");
-const { authenticateUser } = require('../middleware/auth');
+const authenticateToken = require('../middleware/authMiddleware');
 
-router.post("/create-payment-intent", authenticateUser, paymentController.createPaymentIntent);
+router.post("/create-payment-intent", authenticateToken, paymentController.createPaymentIntent);
 
 module.exports = router;
